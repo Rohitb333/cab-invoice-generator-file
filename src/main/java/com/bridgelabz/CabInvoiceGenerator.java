4821dcae182c60637ccd58f354796f47046b0702
Rohit <rohitb333@gmail.com>
2022-04-15 19:37:27 +0530

    Added Multiple Rides File

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -6,11 +6,19 @@ public class CabInvoiceGenerator {
     public int MIN_FARE = 5;
     double totalFare;
 
-    public double CalculateFare(double DISTANCE, int TIME) {
+    public double CalculateFare(double distance, int time) {
 
-        totalFare = COST_PER_KM * DISTANCE + COST_PER_MINUTE * TIME;
+        totalFare = COST_PER_KM * distance + COST_PER_MINUTE * time;
         if(totalFare < MIN_FARE)
             totalFare = MIN_FARE;
+        return totalFare;
+    }
+
+    public double CalculateFare(Rides[] rides) {
+        double totalFare = 0.0;
+        for (Rides ride : rides)
+            totalFare = totalFare + this.CalculateFare(ride.distance, ride.time);
+
         return totalFare;
     }
 }
