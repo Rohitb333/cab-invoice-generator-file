db62bc215eb118f296d8aca621b44efe9bdafb53
Rohit Bansod <97388524+Rohitb333@users.noreply.github.com>
2022-04-18 22:41:44 +0530

    Update CabInvoiceGenerator.java

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -3,16 +3,16 @@ package com.bridgelabz;
 public class CabInvoiceGenerator {
 
     RideRepository rideRepository = new RideRepository();
-    private final int COST_PER_KM = 10;
-    private final int COST_PER_MINUTE = 1;
-    public int MIN_FARE = 5;
+    private final int costPerKm = 10;
+    private final int costPerMin = 1;
+    public int minFare = 5;
     double totalFare;
 
     public double calculateFare(double distance, int time) {
 
-        totalFare = COST_PER_KM * distance + COST_PER_MINUTE * time;
-        if(totalFare < MIN_FARE)
-            totalFare = MIN_FARE;
+        totalFare = costPerKm * distance + costPerMin * time;
+        if(totalFare < minFare)
+            totalFare = minFare;
         return totalFare;
     }
 
